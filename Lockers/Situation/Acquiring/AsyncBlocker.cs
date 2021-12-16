2045ca61716b8ad33fc2346a931ecf5dd2679f32
Uzivatel919 <Uzivatel919@Seznam.cz>
2021-12-16 16:21:10 +0100

    AsyncBlocker + Unblokcer

---
+++ Lockers/Situation/Acquiring/AsyncBlocker.cs
@@ -0,0 +1,42 @@
+﻿using Software9119.Lockers.Extensions;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Software9119.Lockers;
+
+/// <summary>
+/// Provides non-blocking access synchronization, i.e. does not call <see cref="Thread.MemoryBarrier"/>.
+/// </summary>
+sealed public class AsyncBlocker : IDisposable
+{
+  readonly private AutoResetEvent are = new(true);
+  private bool disposed;
+
+  /// <remarks>
+  /// Use <see cref="Timeout.InfiniteTimeSpan"/> for no timeout and <see cref="TimeSpan.Zero"/> for immediate timeout.
+  /// </remarks>
+  async public Task<Unblocker> AutoBlock ( CancellationToken ct, TimeSpan maxWaitTime, TaskScheduler scheduler )
+  {
+    if (scheduler is null)
+      throw new ArgumentNullException (nameof (scheduler));
+
+    if (await are.WaitOneAsync (ct, maxWaitTime, scheduler))
+      return new Unblocker (are);
+
+    return default;
+  }
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+  public void Dispose ()
+  {
+    if (disposed)
+      return;
+
+    are.Dispose ();
+
+    disposed = true;
+  }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
---
+++ Lockers/Situation/Acquiring/Unblocker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+
+namespace Software9119.Lockers;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public struct Unblocker : IDisposable
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+{
+
+  readonly private EventWaitHandle ewh;
+  private bool disposed;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+  public Unblocker ( EventWaitHandle ewh )
+  {
+    this.ewh = ewh;
+    disposed = false;
+  }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
+  /// <returns>
+  /// Whether there is <see cref="EventWaitHandle"/>
+  /// that can be <see cref="EventWaitHandle.Set"/>.
+  /// </returns>
+  /// <remarks>
+  /// When blocking started, returns <see langword="true"/>.
+  /// </remarks>
+  [MemberNotNullWhen (true, nameof (ewh))]
+  public bool CanUnblock => ewh is not null;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+  public void Dispose ()
+  {
+    if (disposed)
+      return;
+
+    _ = Unblock ();
+
+    disposed = true;
+  }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+  public bool Unblock () => CanUnblock && ewh.Set ();
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
